0eab1454f577317fa981e80bc53b36dc97ea8800
Ha Vien Duong <havienduong@gmail.com>
2020-02-29 22:19:13 +0700

    Rename object
    Rename object of neighbor position

--- grid.h
+++ grid.h
@@ -2,7 +2,7 @@
 #define GRID_H
 #include <cell.h>
 #include <rule.h>
-#include <neightbor_position.h>
+#include <neighbor_position.h>
 #include <vector>
 
 class Grid
@@ -10,13 +10,13 @@ class Grid
     int width;
     int height;
     vector<Cell*> cells;
-    vector<NeightborPosition*> neigtborPositions;
+    vector<NeighborPosition*> neighborPositions;
     Rule *rule;
 
     vector<Cell*> getNeighbors(Cell const *cell);
 
 public:
-    Grid(int width,int height, vector<NeightborPosition*> neigtborPositions, Rule const *Rule); // a.V-Duong
+    Grid(int width,int height, vector<NeighborPosition*> neigtborPositions, Rule const *Rule); // a.V-Duong
 
     Cell getCell(int x, int y); //a.V-Duong
 
---
+++ neighbor_position.h
@@ -0,0 +1,15 @@
+#ifndef NEIGHBOR_H
+#define NEIGHBOR_H
+
+
+class NeighborPosition
+{
+    int x, y;
+public:
+    NeighborPosition(int x, int y);
+
+    int getX() const; //a.V-Duong
+    int getY() const; //a.V-Duong
+};
+
+#endif // NEIGHBOR_H
