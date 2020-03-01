8d22a32a9a9076e2798113c32bc26e69a4323e85
tranthuykieu <thuykieu2423@gmail.com>
2020-03-01 22:57:59 +0700

    Kieu: code Simulation::getNeighborPostions

--- simulation.cpp
+++ simulation.cpp
@@ -1,3 +1,4 @@
+
 #include "simulation.h"
 
 Rule *Simulation::getRuleWithRuleName(string ruleName)
@@ -5,9 +6,39 @@ Rule *Simulation::getRuleWithRuleName(string ruleName)
     //TODO: get rule from list rule with rule name (m.duong)
 }
 
-vector<NeighborPosition *> Simulation::getNeighborPostions(string neightborPostionText)
+vector<NeighborPosition *> Simulation::getNeighborPostions(string neighborPostionText)
 {
     //TODO: get NeighborPosition from neightborPostionText (t.kieu)
+    // an exampple of neighorPositionText: 1 0\n0 -1\n2 3\n2 -1
+
+    vector<string> neighbor_list_str;
+    vector<NeighborPosition *> neighbor_list; 
+
+    while (neighborPostionText.length() > 4 ) {
+        // value "4": need to fix 
+        // still brainstorming :3
+        string temp = neighborPostionText.substr(0, neighborPostionText.find("\n"));
+        neighbor_list_str.push_back(temp);
+        neighborPostionText.erase(0,neighborPostionText.find("\n") + 1);
+    }
+    neighbor_list_str.push_back(neighborPostionText);
+
+    for (int i = 0; i <= neighbor_list_str.size() - 1; i++) {
+        int blank_position = neighbor_list_str[0].find(" ");
+        string x_temp = neighbor_list_str[i].substr(0,blank_position);
+        neighbor_list_str[i].erase(0, blank_position + 1);
+        string y_temp = neighbor_list_str[i];
+
+        int x = stoi(x_temp);
+        int y = stoi(y_temp);
+
+        NeighborPosition a(x, y);
+        neighbor_list.push_back(&a);
+
+        return neighbor_list;
+    } 
+    
+    
 }
 
 void Simulation::readInitValueGrid(const string path)
@@ -39,7 +70,8 @@ Simulation::Simulation(Configuration *config)
 
 Simulation::~Simulation()
 {
-    // TODO: delete rules; (t.kieu)
+    // TODO: delete rules; (t.kieu) -- done
+    rules.erase(rules.begin(), rules.end());
 }
 
 void Simulation::run()
@@ -53,8 +85,8 @@ void Simulation::run()
 
     this->grid = new Grid(this->config->getWidth(),this->config->getHeight(), neighborPositions, rule);
 
-    //TODO: foreach times (int time;) (t.kieu)
-
+    //TODO: foreach times (int time;) (t.kieu) -- done??
+    for(int time = 1; time <= this->config->getLimitGeneration(); time++) 
     {
         this->grid->generation();
         string file_output_name = this->config->getDirectoryOutputValuePath() + "/" + to_string(time) + ".txt";
