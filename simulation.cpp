6d699318cf23c23a42eb7765cc597bd8e0c57999
Billy-Tran1804 <60574479+Billy-Tran1804@users.noreply.github.com>
2020-03-01 23:22:38 +0700

    Update simulation.cpp

--- simulation.cpp
+++ simulation.cpp
@@ -1,4 +1,6 @@
 #include "simulation.h"
+#include <iostream>
+#include <fstream>
 
 vector<Rule *> Simulation::getRules() const
 {
@@ -25,20 +27,52 @@ vector<NeighborPosition *> Simulation::getNeighborPostions(string neightborPosti
     //TODO: get NeighborPosition from neightborPostionText (t.kieu)
 }
 
+//BanTQ
 void Simulation::readInitValueGrid(const string path)
 {
-    //TODO: get input value file with height and width in config, after set value state each cell (q.ban)
-
-
-
-    State *state = this->states[value]; // value get in file;
-    //TODO: foreach cell with x, y (q.ban)
-    this->grid->getCell(x, y)->setState(state);
+    unsigned m_width;
+    unsigned m_height;
+    std::ifstream ifs(path);
+    if( !ifs.is_open() ) {
+    std::cerr<<"Unable to open "<<path<<". Exiting ..."<<std::endl;
+    exit(-1);
+    }
+    ifs>>m_width>>m_height;
+    this->config->setWidth(m_width);
+    this->config->setHeight(m_height);
+    //read matrix from the input file and set the value to the grid
+    int A[m_width][m_height];
+    for (int i = 0; i <this->config->getWidth(); ++i)
+    {
+        for (int j = 0; j < config->getHeight(); ++j)
+        {
+            ifs >> A[i][j];
+            State *state = this->states[A[i][j]];
+            this->grid->getCell(i,j)->setState(state);
+        }
+    }
+    ifs.close();
+    //State *state = this->states[value]; // value get in file;
+    //TODO: for each cell with x, y (q.ban)
+    //this->grid->getCell(x, y)->setState(state);
 }
 
 void Simulation::writeValueGrid(const string path)
 {
-    //TODO: write state of cell to file (q.ban)
+    //TODO: write state of cell to file - BanTQ
+    
+    std::ofstream ofs(path);
+    int A[this->config->getWidth()][config->getHeight()];
+
+    //write to file
+    for (int i = 0; i <this->config->getWidth(); ++i)
+    {
+        for (int j = 0; j < config->getHeight(); ++j)
+        {
+            ofs << this->grid->getCell(i,j)->getState << std::endl;
+        }
+    }
+
 }
 
 void Simulation::getRulesFromFile(string path)
