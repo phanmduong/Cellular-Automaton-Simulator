bde3c73298ddc83057aeb7bbbe9a961061e5cbc6
Billy-Tran1804 <60574479+Billy-Tran1804@users.noreply.github.com>
2020-03-02 20:24:21 +0700

    Update user_rule.cpp
    Add Star Wars rule

--- rule/user_rule.cpp
+++ rule/user_rule.cpp
@@ -31,6 +31,31 @@ public:
     }
 };
 
+class StarWars: public Rule
+{
+    public:
+    StarWars(): Rule((string) "Star Wars") {
+
+    }
+    ~StarWars() {}
+    virtual State* excuteRule(const Cell *cell, vector<Cell*> neighbors, vector<State *> states) {
+        State* storeState;
+        for (int i = 0; i < size(states); ++i) {
+            if (cell->getState() == states[i]) {
+                int n = 0;
+                for (int j = 0; j < size(neighbors);++j) {                    
+                    if (neighbors[j]->getState() == cell->getState()) {
+                        n += 1;
+                    }
+
+                }
+                if (n != 3 || n != 4 || n != 5) {
+                    return cell->getState();
+                }
+            }
+        }
+    }
+} 
 extern "C" void initRules(){
     registerRule(new GameOfLife());
     registerRule(new GameOfLife2());
