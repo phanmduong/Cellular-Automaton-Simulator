804db3882559448163339623f0b91c82f8d604db
tranthuykieu <thuykieu2423@gmail.com>
2020-03-02 20:27:43 +0700

    code bombers rule

--- rule/user_rule.cpp
+++ rule/user_rule.cpp
@@ -84,9 +84,38 @@ public:
     }
 };
 
+
+
+class Bombers: public Rule {
+    // 345/24/25
+    // 8 neighbors
+
+    int RULE_SURVIVE[3] = {3,4,5};
+    int RUlE_BIRTH
+
+
+    public:
+        Bombers(): Rule((string) "Bombers") {}
+        ~Bombers() {}
+
+        virtual State* excuteRule(const Cell *cell, vector<Cell*> neighbors, vector<State *> states){
+            int cnt_live_neighbor = 0;
+            for (int i = 0; i < neighbors.size(); i++) {
+                if (neighbors[i]->getState() != cell->getState()) cnt_live_neighbor++;
+            }
+            if (cnt_live_neighbor >= 3 && cnt_live_neighbor <=5) {
+                
+            }
+            
+            return states[2];
+        }
+        
+};
+
 extern "C" void initRules(){
     registerRule(new ConwaysGameOfLife());
     registerRule(new GameOfLife2());
+    registerRule(new Bombers()); //t.kieu
 }
 
 extern "C" vector<Rule*> getAllRules(){
--- user_rule.cpp
+++ user_rule.cpp
@@ -31,31 +31,10 @@ public:
     }
 };
 
-class Bombers: public Rule {
-    // 345/24/25
-    // 8 neighbors
-    public:
-        Bombers(): Rule((string) "Bombers") {}
-        ~Bombers() {}
-
-        virtual State* excuteRule(const Cell *cell, vector<Cell*> neighbors, vector<State *> states){
-            int cnt_live_neighbor = 0;
-            for (int i = 0; i < neighbors.size(); i++) {
-                if (neighbors[i]->getState() != cell->getState()) cnt_live_neighbor++;
-            }
-            if (cnt_live_neighbor >= 3 && cnt_live_neighbor <=5) {
-                
-            }
-            
-            return states[2];
-        }
-        
-}
 
 extern "C" void initRules(){
     registerRule(new GameOfLife());
     registerRule(new GameOfLife2());
-    registerRule(new Bombers()); //t.kieu
 }
 
 extern "C" vector<Rule*> getAllRules(){
